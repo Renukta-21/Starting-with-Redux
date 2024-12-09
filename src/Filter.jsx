238c1904dd48964cb69584acb54fff99979fb9a2
Daniel Urbina <edu211004@gmail.com>
2024-12-08 19:26:11 -0600

    action creators

--- src/Filter.jsx
+++ src/Filter.jsx
@@ -1,7 +1,11 @@
+import { useDispatch, useSelector } from "react-redux"
+import {changeFilter} from "./reducers/filterReducer"
+
 const Filter = () => {
-    const handleChange = (event) => {
-      // input-field value is in variable event.target.value
-    }
+    const dispatch = useDispatch()
+    const currentFilter = useSelector(state=> state.filter)
+    const handleChange = (e) => dispatch(changeFilter(e.target.value))
+    console.log(currentFilter)
     const style = {
       marginBottom: 10
     }
